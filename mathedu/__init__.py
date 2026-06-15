75c672666637ecbf0d3da9d5ce57bcfa0d3f2412
Sorry0o0 <359397870@qq.com>
2026-06-15 16:57:02 +0800

    Initial commit: Add mathedu/__init__.py

---
+++ mathedu/__init__.py
@@ -0,0 +1,8 @@
+"""
+MathEdu Toolkit — AI-Powered Elementary Mathematics Education Toolkit
+Copyright (c) 2025-2026 Zhang Rui
+MIT License
+"""
+
+__version__ = "0.4.2"
+__author__ = "Zhang Rui (张睿)"
